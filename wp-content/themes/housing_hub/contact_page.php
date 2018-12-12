8c95ca1c835994234d5cbfa0888711d43138746f
Sophie <sophiearoberts3@gmail.com>
2018-12-12 13:18:27 +0000

    Initial commit

---
+++ wp-content/themes/housing_hub/contact_page.php
@@ -0,0 +1,11 @@
+<?php
+	/*
+	* Template Name: Contact
+	*/ 
+?>
+<?php get_header() ?>
+
+
+<?php get_footer() ?>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/main.js"></script>
---
+++ wp-content/themes/housing_hub/header.php
@@ -0,0 +1,21 @@
+<head>
+<!-- 	<meta name="description" content="We work with all different kinds of people. Whether you're a new business wanting the whole works. Or a small organisation looking for a simple website. We will work with you to provide a service suited to your needs.">
+	<meta name="keywords" content="wesite, website build, website develop, website design, web, web build, web develop, web design, social media, branding, design, lancaster, lancashire, cumbria, north west, wordpress">
+ -->	<meta name="robots" content="index,follow">
+	<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/header.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/main.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/phone.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/footer.css">
+	<link href="https://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet">
+	<title>Student Housing Hub</title>
+
+</head>
+
+
+
+<div class="nav">
+	<a href="<?php echo home_url(); ?>" class="logo"></a>
+	<div class="nav_menu">
+		<?php wp_nav_menu() ?>
+	</div>
+</div>
